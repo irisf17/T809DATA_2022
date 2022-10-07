7ee496b5a4bb199da5c49c255f2df17f2d7c8b3e
irisf17 <irisf17@ru.is>
2022-10-07 17:15:34 +0000

    7 and 8 done, not independent

--- 07_linear_models/template.py
+++ 07_linear_models/template.py
@@ -15,6 +15,28 @@ from tools import load_regression_iris
 from scipy.stats import multivariate_normal
 
 
+# GOAL = find the last feature in iris data with regression
+# part 1.1 : scipy.stats.... does the gaussian formula
+# gives out the phi matrix
+
+# part 1.2
+# goes lower because of 3 classes
+
+
+# part 1.3
+# outputs the weights
+
+# part 1.4
+# takes in weights, predicts the values
+
+# part 1.5
+# plot results, calculate error, the min, the max, compare...
+
+# INDE
+# Does the method work well? from above 1.5 we can see, then how can we make it better?
+# add more basis functions?..
+
+
 def mvn_basis(
     features: np.ndarray,
     mu: np.ndarray,
@@ -25,21 +47,38 @@ def mvn_basis(
     The function transforms (possibly many) data vectors <features>
     to an output basis function output <fi>
     Inputs:
-    * features: [NxD] is a data matrix with N D-dimensional
+    * features: [NxD] 150x3 is a data matrix with N D-dimensional
     data vectors.
-    * mu: [MxD] matrix of M D-dimensional mean vectors defining
+    * mu: [MxD] 10x3 matrix of M D-dimensional mean vectors defining
     the multivariate normal distributions.
     * sigma: All normal distributions are isotropic with sigma*I covariance
-    matrices (where I is the MxM identity matrix)
+    matrices (where I is the MxM 10x10 identity matrix)
     Output:
     * fi - [NxM] is the basis function vectors containing a basis function
     output fi for each data vector x in features
     '''
-    ...
+    N,D = features.shape
+    M = mu.shape[0]
+    I = np.identity(D)
+    # sigma_m = sigma*I #3x3 matrix
+    fi = np.zeros((N, M))
+
+    for i in range(fi.shape[1]):
+        fi[:, i] = multivariate_normal.pdf(features, mu[i,:], sigma)
+
+    return fi
 
 
-def _plot_mvn():
-    ...
+def _plot_mvn(fi: np.ndarray):
+
+    # X,t = load_regression_iris()
+
+    for i in range(fi.shape[1]):
+        plt.plot(fi[:,i])
+    
+    plt.legend(['basis_1', 'basis_2', 'basis_3', 'basis_4', 'basis_5', 'basis_6', 'basis_7', 'basis_8', 'basis_9', 'basis_10'])
+    plt.title("Outputs for each basis functions")
+    plt.show()
 
 
 def max_likelihood_linreg(
@@ -57,7 +96,10 @@ def max_likelihood_linreg(
 
     Output: [Mx1], the maximum likelihood estimate of w for the linear model
     '''
-    ...
+    N,M = fi.shape
+    I = np.identity(M)
+    wml = np.linalg.inv(fi.T.dot(fi) + lamda*I).dot(fi.T.dot(targets))
+    return wml
 
 
 def linear_model(
@@ -78,4 +120,69 @@ def linear_model(
 
     Output: [Nx1] The prediction for each data vector in features
     '''
-    ...
+    return np.matmul(mvn_basis(features, mu, sigma), w)
+
+def _square_error(real, pred):
+    sqr_error = []
+    for i in range(len(real)):
+        foo = real[i] - pred[i]
+        error = np.power(foo, 2)
+        sqr_error.append(error)
+    return sqr_error
+
+if __name__ == '__main__':
+    #  PART 1.1
+
+    X,t = load_regression_iris()
+    N,D = X.shape
+    M, sigma = 10, 10
+    mu = np.zeros((M, D))
+    for i in range(D):
+        mmin = np.min(X[i, :])
+        mmax = np.max(X[i, :])
+        mu[:, i] = np.linspace(mmin, mmax, M)
+    fi = mvn_basis(X, mu, sigma)
+    # print(fi)
+
+    # PART 1.2
+    _plot_mvn(fi)
+
+    # PART 1.3
+    lamda = 0.001
+    wml = max_likelihood_linreg(fi, t, lamda)
+    # print(wml)
+
+    # PART 1.4
+    prediction = linear_model(X, mu, sigma, wml)
+    print(prediction)
+
+    # PART 1.5
+    # comparing predicted features to actual features
+
+    plt.plot(range(0, len(prediction)), prediction)
+    plt.plot(range(0, len(t)), t)
+    plt.legend(["Predicted", "Actual"])
+    plt.title("Predicted vs Actual values")
+    plt.xlabel("Iterations")
+    plt.ylabel("Features")
+    plt.show()
+
+    # need to find the square error and plot it to show if getting closer to real value
+    sqr_error = _square_error(t, prediction)
+
+    sqr_e_1 = np.sum(sqr_error[0:49])/len(sqr_error[0:49])
+    sqr_e_2 = np.sum(sqr_error[50:99])/len(sqr_error[50:99])
+    sqr_e_3 = np.sum(sqr_error[100:150])/len(sqr_error[100:150])
+    print(sqr_e_1)
+    print(sqr_e_2)
+    print(sqr_e_3)
+
+    plt.plot(range(0,len(sqr_error)), sqr_error)
+    plt.title("Mean-sqr-Error")
+    plt.xlabel("Iterations")
+    plt.ylabel("Sqr Error")
+    # plt.text(10, .050, r'$\mu=0.24\ $')
+    plt.text(10, 0.5, r'$\mu=0.24\ $')
+    plt.text(60, 1.2, r'$\mu=0.36\ $')
+    plt.text(115, .050, r'$\mu=2.08\ $')
+    plt.show()
--- 08_SVM/template.py
+++ 08_SVM/template.py
@@ -8,7 +8,35 @@
 # automatically.  We ONLY want the functions as stated in the README.md.
 # Make sure to comment out or remove all unnecessary code before submitting.
 
+# part 1.1
+# making small data with blobs.
 
+
+# part 1.2
+
+
+# part 1.3
+
+
+# part 1.4
+
+
+# part 1.5
+# c lowers then more stack variables?
+
+
+# part 2.1
+# we can start using more complicated data set
+# 
+
+
+# part 2.2
+
+
+# part 2.3
+
+
+from calendar import c
 from tools import plot_svm_margin, load_cancer
 from sklearn import svm
 from sklearn.datasets import make_blobs
@@ -19,9 +47,17 @@ import matplotlib.pyplot as plt
 
 
 def _plot_linear_kernel():
-    X, t = make_blobs(...)
-    ...
-    plot_svm_margin(...)
+    X,t = make_blobs(n_samples=40, centers=2, random_state=6)
+    clf = svm.SVC(C=1000, kernel= 'linear')
+    clf.fit(X,t)
+    # plot of for 2 points
+    # plot_svm_margin(clf, X[0:2,:], t[0:2])
+    # print(X[0:2,:])
+    plt.title("SVM hyperplane")
+    plot_svm_margin(clf, X, t)
+    # There are 20 support vectors for each class
+    # The decision boundary is linear.
+    return X,t
 
 
 def _subplot_svm_margin(
@@ -45,27 +81,90 @@ def _subplot_svm_margin(
     # similar to tools.plot_svm_margin but added num_plots and
     # index where num_plots should be the total number of plots
     # and index is the index of the current plot being generated
-    ...
+    plt.subplot(1, num_plots, index)
+    plt.scatter(X[:, 0], X[:, 1], c=t, s=30, cmap=plt.cm.Paired)
+
+    # plot the decision function
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # create grid to evaluate model
+    xx = np.linspace(xlim[0], xlim[1], 30)
+    yy = np.linspace(ylim[0], ylim[1], 30)
+    YY, XX = np.meshgrid(yy, xx)
+    xy = np.vstack([XX.ravel(), YY.ravel()]).T
+
+    Z = svc.decision_function(xy).reshape(XX.shape)
+
+    # plot decision boundary and margins
+    ax.contour(
+        XX, YY, Z,
+        colors='k', levels=[-1, 0, 1],
+        alpha=0.5, linestyles=['--', '-', '--'])
+
+    # plot support vectors
+    ax.scatter(
+        svc.support_vectors_[:, 0],
+        svc.support_vectors_[:, 1],
+        s=100, linewidth=1, facecolors='none', edgecolors='k')
 
 
 def _compare_gamma():
+    plt.figure()
     X, t = make_blobs(n_samples=40, centers=2, random_state=6)
 
-    clf = svm.SVC(...)
-    ...
+    clf = svm.SVC(C=1000, kernel= 'rbf', gamma='scale')
+    clf.fit(X,t)
     _subplot_svm_margin(clf, X, t, 3, 1)
+    plt.title("gamma = default")
 
-    clf = ...
-    ...
+    clf = svm.SVC(C=1000, kernel= 'rbf', gamma=0.2)
+    clf.fit(X,t)
     _subplot_svm_margin(clf, X, t, 3, 2)
+    plt.title("gamma = 0.2")
 
-    ...
-
+    clf = svm.SVC(C=1000, kernel= 'rbf', gamma=2)
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 3, 3)
+    plt.title("gamma = 2")
     plt.show()
 
-
 def _compare_C():
-    ...
+    plt.figure()
+    X, t = make_blobs(n_samples=40, centers=2, random_state=6)
+
+    clf = svm.SVC(C=1000, kernel= 'linear')
+    clf.fit(X,t)
+    print(len(clf.support_vectors_))
+    _subplot_svm_margin(clf, X, t, 5, 1)
+    plt.title("C = 1000")
+
+    clf = svm.SVC(C=0.5, kernel= 'linear')
+    clf.fit(X,t)
+    print(len(clf.support_vectors_))
+    _subplot_svm_margin(clf, X, t, 5, 2)
+    plt.title("C = 0.2")
+
+    clf= svm.SVC(C=0.3, kernel= 'linear')
+    clf.fit(X,t)
+    print(len(clf.support_vectors_))
+    _subplot_svm_margin(clf, X, t, 5, 3)
+    plt.title("C = 0.3")
+
+    clf= svm.SVC(C=0.05, kernel= 'linear')
+    clf.fit(X,t)
+    print(len(clf.support_vectors_))
+    _subplot_svm_margin(clf, X, t, 5, 4)
+    plt.title("C = 0.05")
+
+    clf= svm.SVC(C=0.0001, kernel= 'linear')
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 5, 5)
+    print(len(clf.support_vectors_))
+    plt.title("C = 0.0001")    
+
+    plt.show()
 
 
 def train_test_SVM(
@@ -73,7 +172,7 @@ def train_test_SVM(
     X_train: np.ndarray,
     t_train: np.ndarray,
     X_test: np.ndarray,
-    t_test: np.ndarray,
+    t_test: np.ndarray
 ):
     '''
     Train a configured SVM on <X_train> and <t_train>
@@ -82,4 +181,131 @@ def train_test_SVM(
 
     This function should return (accuracy, precision, recall)
     '''
-    ...
+    svc.fit(X_test, t_test)
+    predicted = svc.predict(X_test)
+
+    acc = accuracy_score(t_test, predicted)
+    prec = precision_score(t_test, predicted)
+    rec = recall_score(t_test, predicted)
+
+    print(f"Accuracy score: {acc}")
+    print(f"Precision score: {prec}")
+    print(f"Recall score: {rec}")
+    return acc, prec, rec
+
+def comparevirkarekki(
+    svc,
+    X_train: np.ndarray,
+    t_train: np.ndarray,
+    X_test: np.ndarray,
+    t_test: np.ndarray,
+    title: str
+):
+    '''
+    Train a configured SVM on <X_train> and <t_train>
+    and then measure accuracy, precision and recall on
+    the test set
+
+    This function should return (accuracy, precision, recall)
+    '''
+    svc.fit(X_test, t_test)
+    predicted = svc.predict(X_test)
+
+    acc = accuracy_score(t_test, predicted)
+    prec = precision_score(t_test, predicted)
+    rec = recall_score(t_test, predicted)
+
+    plt.plot(range(0,len(t_test)),t_test)
+    plt.plot(range(0,len(predicted)),predicted)
+    plt.title(title)
+    plt.show()
+
+    print(f"Accuracy score: {acc}")
+    print(f"Precision score: {prec}")
+    print(f"Recall score: {rec}")
+    return acc, prec, rec
+
+def compare_INDE():
+    plt.figure()
+    X,t = make_blobs(n_samples=100, centers=2, random_state=6)
+    clf = svm.SVC(C=0.01, kernel= 'rbf', gamma=2)
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 5, 1)
+    plt.title("C = 0.01, gamma=2")
+
+    clf = svm.SVC(C=50, kernel= 'rbf', gamma=1)
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 5, 2)
+    plt.title("C = 500, gamma=1")
+
+    clf= svm.SVC(C=10, kernel= 'rbf', gamma=0.5)
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 5, 3)
+    plt.title("C = 10, gamma=0.5")
+
+    clf= svm.SVC(C=0.05, kernel= 'rbf', gamma=0.5)
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 5, 4)
+    plt.title("C = 0.05, gamma=0.5")
+
+    clf= svm.SVC(C=0.0001, kernel= 'rbf', gamma=0.02)
+    clf.fit(X,t)
+    _subplot_svm_margin(clf, X, t, 5, 5)
+    plt.title("C = 0.0001, gamma=0.02")    
+
+    plt.show()
+
+if __name__ == '__main__':
+    # Part 1.1
+
+    _plot_linear_kernel()
+
+    # Part 1.3
+    _compare_gamma()
+
+    # Part 1.5
+    _compare_C()  
+
+    # Part 2.1
+    '''
+    (X_train, t_train), (X_test, t_test) = load_cancer()
+    # default kernel = 'rbf'
+    svc = svm.SVC(C=1000) 
+
+    train_test_SVM(svc, X_train, t_train, X_test, t_test)
+
+    # Part 2.2
+    # Linear
+    svc = svm.SVC(C=1000, kernel='linear')
+    print("linear")
+    train_test_SVM(svc, X_train, t_train, X_test, t_test)
+    # radial basis
+    print("radial basis")
+    svc = svm.SVC(C=1000, kernel='rbf')
+    train_test_SVM(svc, X_train, t_train, X_test, t_test)
+    # polynomial
+    print("polynomial")
+    svc = svm.SVC(C=1000, kernel='poly')
+    train_test_SVM(svc, X_train, t_train, X_test, t_test)
+    '''
+    # Indepentend Section
+    # Comparing visually different parameters on the cancer dataset
+    # Hyper-parameters like C or Gamma control how 
+    # wiggling the SVM decision boundary could be.
+    # using default kernel = rbf
+    # (X_train, t_train), (X_test, t_test) = load_cancer()
+    # print(X_test.shape)
+    # print(t_test.shape)
+
+    # clf = svm.SVC(C=500, kernel= 'poly', gamma=1)
+    # clf.fit(X_test,t_test)
+    # svc = svm.SVC(C=1000, kernel='poly', gamma=5)
+    # compare(svc, X_train, t_train, X_test, t_test, "c=1000, gamma = 5")
+
+
+    # Using larger blob to compare
+    # compare_INDE()
+
+
+
+
